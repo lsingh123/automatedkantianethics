779a710bba5028e3edc07a2e13c44afba728e422
Lavanya Singh <lsingh@college.harvard.edu>
2021-04-02 12:51:39 -0400

    rough paper intro + kroy progress

--- kroy.thy
+++ kroy.thy
@@ -3,11 +3,13 @@ theory kroy
 
 begin
 
-text "This theory will contain a formalization of the CI based on Moshe Kroy's partial formalization. @{cite kroy}}"
+text "This theory will contain a formalization of the CI based on Moshe Kroy's partial formalization. @{cite kroy}"
 
-section "The Substitution Operator"
+section "Kroy's Formalization of the Categorical Imperative"
 
-subsection Definition
+subsection "The Substitution Operator"
+
+subsubsection Definition
 
 typedecl s \<comment>\<open>s is the type for a ``subject", like the subject of a sentence\<close>
 \<comment>\<open>Intuitively, we need some notion of ``x does action", which we can write as ``x is the subject of the sentence 'does action'"\<close>
@@ -18,7 +20,7 @@ type_synonym os = "(s \<Rightarrow> t)"
 \<comment>\<open>``P sub (d/e)" can be written as ``S(e)", where S(d) = P\<close>
 \<comment>\<open>So the terms that we substitute into are actually instantiations of an open sentence, and substitution just requires re-instantiating the open sentence with a different subject\<close>
 
-subsection Abbreviations
+subsubsection Abbreviations
 
 abbreviation os_neg::"os \<Rightarrow> os" ("\<^emph>\<not>_")
   where "(\<^emph>\<not>A) \<equiv> \<lambda>x. \<^bold>\<not>(A(x))"
@@ -36,26 +38,79 @@ abbreviation os_permissible::"os\<Rightarrow>os" ("\<^emph>P {_}")
   where "\<^emph>P {A} \<equiv> \<lambda>x. P {A(x)}"
 \<comment>\<open>Carmo and Jones don't make much use of permissibility, but we will find it useful here.\<close>
 
-section "Differences Between Kroy's Logic (Kr) and DDL"
+subsection "Differences Between Kroy's Logic (Kr) and DDL"
 
 text "@{cite kroy} uses a different logic than DDL. Let's see if the semantics that Kr requires hold in DDL"
 
-lemma permissible_semantic:
+lemma permissible_semantic_faithful:
   fixes A w
-  assumes "P {A} w"
-  shows "\<exists>x. (\<forall>B. B(x) \<longrightarrow> ob((\<lambda>w. True))(B) \<and> A(x))"
+  shows "P {A} w \<longrightarrow> (\<exists>x. A(x))"
   nitpick[user_axioms] oops
-\<comment>\<open>TBC...this is definitely wrong\<close>
+\<comment>\<open>The most faithful interpretation of Kr is that if A is permissible in a context, then 
+it must be true at some world in that context. Kr operates under the ``deontic alternatives" view, 
+summarized by Solt as ``A proposition of the sort OA is true at the actual world w if and
+only if A is true at every deontic alternative world to t." Under this view, permissible propositions
+are obligated at some deontic alternative, but not at all of them.
 
-section "The Categorical Imperative"
+DDL does not adopt a deontic alternatives view, which is why this proposition seems wildly counterintuitive
+in DDL. In DDL, the ob operator abstracts away the notion of deontic alternatives and completely determines
+obligations. Even if one belives that permissible statements should be true at some deontic alternative, 
+it's not clear that permissible statements must be realized at some world, hence the failure of this 
+lemma in DDL.\<close>
+\<comment>\<open>Nitpick found a counterexample for card i = 1:
 
-abbreviation CI::"bool" where "CI \<equiv> \<forall>w. (\<exists>p. P {p}w \<longrightarrow> (\<forall>x. P {x}w))"
+  Free variable:
+    A = ($\lambda x. \_$)($i_1$ := False)\<close>
+
+lemma permissible_semantic_faithful_2:
+  fixes A w
+  shows "P {A} w \<longrightarrow> (\<exists>x. O {A} x)"
+  nitpick[user_axioms] oops
+\<comment>\<open>Nitpick found a counterexample for card i = 1:
+
+  Free variable:
+    A = ($\lambda x. \_$)($i_1$ := False)\<close>
+\<comment>\<open>This is the most clear lemma that we would expect to hold under the deontic alternatives view. The 
+fact that it doesn't shows DDL is not a logic of deontic alternatives. There are pros and cons to this
+approach. The deontic alternatives view is quite simple to visualize and offers clear intuition. On
+the other hand, DDL's ob function can encode more complex relations than the deontic alternatives view,
+and can encode these in a more intuitive manner. The notion of a ``deontically perfect alternative"
+is a squishy one, and an ob function more directly captures the idea of obligation.\<close>
+
+
+lemma permissible_semantic_vacuous:
+  fixes A w
+  shows "P {A} w \<longrightarrow> (\<exists>x. ob(x)(A))"
+  nitpick[user_axioms] oops
+\<comment>\<open>Kr does not allow vasuously permissible statements -> if something is permissible it has to be obligated 
+at some deontically perfect alternative. In DDL, this can be roughly translated as, if A is permissible, 
+it is obligated in some context.\<close>
+\<comment>\<open>Nitpick found a counterexample for card i = 1:
+
+  Free variable:
+    A = ($\lambda x. \_$)($i_1$ := False)\<close>
+\<comment>\<open>In order to make this true, we'd have to require that everything is either obligatory or 
+prohibited somewhere. But as found in the buggy version of DDL, that breaks everything and destroys the 
+notion of permissibility everywhere. I am going to allow for vacuous permissibility. If something breaks later
+in Kr, it may be because of this.\<close>
+
+subsection "The Categorical Imperative"
+
+abbreviation CI::"bool" where "CI \<equiv> \<forall>w A. ((\<exists>p. (\<^emph>P {A}p )w) \<longrightarrow> (\<forall>x. (\<^emph>P {A}x)w))"
+\<comment>\<open>This is Kroy's formalization of the FUL in DDL. Recall that the FUL says
+``act only in accordance with that maxim through which you can at the same time will that it become a universal law" @{cite groundwork}
+Kroy interprets this to mean that if an action is permissible for me, then it must be permissible for everyone.
+This formalizes an important moral intuition - the CI prohibits free-riding. No one is a moral exception\<close>
 
 lemma CI:
-  fixes Lav::s
-  fixes P::os
-  shows " \<forall>w. (\<exists>p. P {p}w \<longrightarrow> (\<forall>x. P {x}w))"
-  oops
-\<comment>\<open>ugh\<close>
+  shows CI
+  nitpick[user_axioms] oops
+\<comment>\<open>Nitpick found a counterexample for card s = 2 and card i = 2:
+
+  Skolem constants:
+    A = ($\lambda x. \_$)($s_1$ := ($\lambda x. \_$)($i_1$ := True, $i_2$ := True), $s_2$ := ($\lambda x. \_$)($i_1$ := False, $i_2$ := False))
+    p = $s_1$
+    x = $s_2$
+This formalization doesn't hold in DDL. Good - this means that adding it as an axiom will change the logic.\<close>
 
 end
--- paper.thy
+++ paper.thy
@@ -5,6 +5,75 @@
 
 section Introduction
 
-text "my name is Lavanya"
+text \<open>Leibniz dreamed of modelling all knowledge in the language of formal logic, so that all 
+reasoning could be automated. As machines become increasingly capable of mathematical, strategic, 
+and scientific reasoning and Leibniz's dream becomes closer to reality, one key gap remains.
+Machines lack the ability to perform any meaningful kind of ethical reasoning. Computational 
+ethics is a young but attractive field for two primary reasons. First, the proliferation of 
+artifically intelligence and autonomous agents is creating and will continue to create a demand for 
+ethical autonomous agents. The call for ``ethical AI" can, in one sense, be answered by the creation 
+of an automated ethical reasoner. Second, just as automated mathematical reasoning allows mathematicians 
+to explore new proofs, automated ethical reasoning is a tool that philosophers can use when reasoning 
+about ethics. Many contradictions or paradoxes with an ethical system may not be immediately obvious 
+to the human eye, but can be easily tested using an automated theorem prover.
 
-end
+Modelling ethics without sacrificing the intracacies and complexities of an ethical theory is a 
+challenging computational and philosophical problem. Many ethical AI systems encode ethics as a series 
+of constraints, and they maximize their objective with respect to such constraints. This approach to 
+ethics fails to capture much of the complexity of any plausible ethical system. Any faithful model
+of an ethical theory will require machinery more complex than a single constraint satisfaction problem.
+
+In addition to the computational machinery, computational ethics also requires a sophisticated 
+ethical theory to model. Constraint satisfaction systems often default to some version of utiliarianism, 
+the principle of doing the most good for the most people. Alternatively, they model basic moral 
+principles such as ``do not kill," without modelling the theory that these principles originated from.
+Modelling a more complex ethical theory will not only enable smarter philosophical machines, it will
+also empower philosophers to study more complex ethical issues with the computer's help.
+
+The ideal candidate ethical theory will be both philosophically interesting and easy to 
+formalize. Kantian ethics, often described as ``formal," is such a candidate. The categorical imperative,
+Kant's universal law of morality, is a moral rule that can be used to guide action in all spheres of life.
+Kant's original presentation itself is methodical and formal, and the theory lends itself well to 
+formalization.
+
+In this paper, I present, implement, and test three formalizations of Kant's categorical imperative 
+in the Isabelle/HOL theorem prover. I start with Carmo and Jones' Dyadic Deontic Logic as a base
+logic and model each formalization as an extension of DDL. Section 4.3 implements and tests
+the naive implementation, a control group that is not much stronger than DDL itself. Section 4.4
+examines a more sophisticated implementation inspired by Moshe Kroy's partial formalization of the 
+categorical imperative. Section 4.5 explores ????
+
+I contribute implementations of three different interpretations of the categorical imperative, 
+examples of how each implementation can be used to model and solve ethical scenarios, and tests that
+examine ethical and logical properties of the system, including logical consistency, consistentency
+of obligation, and possibility of permissibility. The implementations themselves are usable models 
+of ethical principles and the tests represent the kind of philosophical work that formalized ethics 
+can contribute.\<close>
+
+section "System Overview"
+
+text \<open>Formalizing an ethical theory requires a choice of formal language to model the theory in, 
+the choice of ethical theory itself, and the choice of theorem prover to implement the formal model.
+I use as my base the LogiKEY Workbench @{cite BP} project, a framework for modelling ethics using 
+deontic logic. Like LogiKEY, I use Carmo and Jones's Dyadic Deontic Logic @{cite CJDDL}. I focus on 
+Kant's categorical imperative, using formalizations inspired by all three formulations of the 
+categorical imperative. All the programming is done in Isabelle/HOL.\<close>
+
+subsection "Dyadic Deontic Logic"
+
+subsection "Kantian Ethics"
+
+subsection "Isabelle/HOL"
+
+section "Details"
+
+subsection "Naive Implementation/Control Group"
+
+subsection "Kroy's Partial Formalization"
+
+section "Related Work"
+
+section "Conclusion"
+
+
+(*<*) end (*>*)
