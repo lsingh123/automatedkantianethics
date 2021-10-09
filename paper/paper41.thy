6da5ced751b8745b5106a7c0e27ae89f0b5faad4
Lavanya Singh <lsingh@college.harvard.edu>
2021-10-09 11:11:11 -0400

    fixed contradiction

--- paper/paper41.thy
+++ paper/paper41.thy
@@ -197,21 +197,128 @@ interesting for my purposes because, when the circumstances do not hold, a maxim
 doesn't really make sense to evaluate a maxim when it's not supposed to be applied. The maxim ``When on Jupiter,
 read a book to one-up your nemesis" is vacuously effective because it can never be disproven.\<close>
 
-abbreviation LHS::"maxim\<Rightarrow>s\<Rightarrow>t" where 
-"LHS \<equiv> \<lambda>M s. (\<lambda>w. (\<forall>p. W M p w))"
+abbreviation universalized::"maxim\<Rightarrow>s\<Rightarrow>t" where 
+"universalized \<equiv> \<lambda>M s. (\<lambda>w. (\<forall>p. W M p w))"
 
 abbreviation not_universalizable :: "maxim\<Rightarrow>s\<Rightarrow>bool" where 
-"not_universalizable \<equiv> \<lambda>M s. (\<Turnstile> (LHS M s \<^bold>\<rightarrow> (\<^bold>\<not> (E M s))))"
+"not_universalizable \<equiv> \<lambda>M s. (\<Turnstile> (universalized M s \<^bold>\<rightarrow> (\<^bold>\<not> (E M s))))"
 \<comment>\<open>The maxim willed by subject $s$ is not universalizable if, for all people $p$, if $p$ wills M, then 
 $M$ is no longer effective for $s$.\<close>
-print_theorems
 
-text \<open> This formalizes Korgsaard's definition of the practical contradiction
+text \<open> Below is my first attempt at formalizing Korgsaard's definition of the practical contradiction
 interpretation:  a maxim is not universalizable 
 if, in the world where the maxim becomes the standard practice (i.e. everyone acts on the maxim), the
 agent's attempt to use the maxim's act to achieve the maxim's goal is frustrated. In other words, if 
 the maxim is universally willed (captured by applying a universal qunatifier and the will function 
 to the maxim on the LHS), then it is no longer effective for the subject $s$ (RHS above). 
+\<close>
+
+abbreviation prohibited::"maxim\<Rightarrow>s\<Rightarrow>t" where 
+"prohibited \<equiv> \<lambda>(c, a, g) s. O{\<^bold>\<not> (will (c,a, g) s) | c}"
+
+abbreviation FUL0::bool where "FUL0 \<equiv> \<forall> c a g s. not_universalizable (c, a, g) s \<longrightarrow> \<Turnstile>((prohibited (c, a, g) s))"
+\<comment>\<open>This representation of the Formula of Universal Law reads, ``For all circumstances, goals, acts, 
+and subjects, if the maxim of the subject performing the act for the goal in the circumstances is not 
+universalizable (as defined above), then, at all worlds, in those circumstances, the subject 
+is prohibited (obligated not to) from willing the maxim.\<close>
+
+lemma "FUL0 \<longrightarrow> False" using O_diamond 
+  using prod.simps(2) split_conv by fastforce
+
+text \<open>FUL0 is not consistent, and sledgehammer is indeed able to show that it implies a contradiction. 
+The axioms that sledgehammer used are O\_diamond, which is @{"thm" "O_diamond"}. This axiom captures 
+the idea that an obligation can't contradict its context. This is particularly problematic if the goal or 
+action of a maxim are equivalent to its circumstances. In other words, if the maxim has already been 
+acted on or the goal has already been achieved, then prohibiting it is impossible. 
+In any model that has at least one term, it is possible to construct a maxim where the circumstances, goal, 
+and act (once a subject acts on it) are all that same term, resulting in a contradiction. 
+
+To get around this, I will exclude what I call ``badly formed maxims," which are those maxims sucht that the goal has already been 
+achieved or the act has already been acted on. Under my formalization, such maxims are 
+not well-formed. To understand why, we can return to Korsgaard's and O'Neill's interpretations of a maxim as a practical
+guide to action. A maxim is a practical principle that guides how we behave in everyday life. A 
+principle of the form ``When you are eating breakfast, eat breakfast in order to eat breakfast," is not 
+practically relevant. No agent would ever need to act on such a principle. It's not necessarily contradictory
+or prohibited, but it is the wrong kind of question to be asking yourself. It is not a 
+well-formed maxim, so the categorical imperative does not apply to it.\<close>
+
+abbreviation well_formed::"maxim\<Rightarrow>s\<Rightarrow>i\<Rightarrow>bool" where 
+"well_formed \<equiv> \<lambda>(c, a, g) s w. (\<not> ( (c \<^bold>\<rightarrow> g) w)) \<and> (\<not> ( (c \<^bold>\<rightarrow> a s) w))"
+\<comment>\<open>This abbreviation formalizes the well-formedness of a maxim for a subject. The goal cannot be 
+already achieved in the circumstances and the subject cannot have already performed the act.\<close>
+
+abbreviation FUL where "FUL \<equiv> \<forall>M::maxim. \<forall>s::s. (\<forall>w. well_formed M s w) \<longrightarrow> (not_universalizable M s \<longrightarrow> \<Turnstile> prohibited M s )"
+\<comment>\<open>Let's try the exact same formalization of the FUL as above, except that it only applies to 
+maxims that are well-formed at every world.\<close>
+
+lemma "FUL"
+  nitpick[user_axioms, falsify=true] oops
+\<comment>\<open>The FUL does not hold in DDL, because nitpick is able to find a model for my system in which it is 
+false. If the FUL were already a theorem of the system, adding it wouldn't make the system any more 
+powerful, so this is the desired result.
+$\color{blue}$ Nitpick found a counterexample for card s = 1 and card i = 1:
+
+  Skolem constants:
+    a = ($\lambda x. \_$)($s_1$ := ($\lambda x. \_$)($i_1$ := False))
+    c = ($\lambda x. \_$)($i_1$ := True)
+    g = ($\lambda x. \_$)($i_1$ := False)
+    $\lambda w$. p = ($\lambda x. \_$)($i_1$ := $s_1$)
+    s = $s_1$ $\color{black}$ \<close>
+
+axiomatization where FUL:FUL
+
+lemma True
+  nitpick[user_axioms, falsify=false] by simp
+\<comment>\<open>The system is still consistent! Nitpick is able to find a model in which all axioms are satisfied, 
+so this version of the FUL is consistent.
+$\color{blue}$ Nitpick found a model for card i = 1 and card s = 1:
+
+  Empty assignment $\color{black}$ \<close>
+
+text \<open>During the process of making FUL0 consistent, I used Isabelle to gain philosophical insights, 
+especially about vacuous maxims. I used Nitpick and Sledgehammer to quickly test if a small tweak 
+to FUL0 fixed the problem or if I was still able to derive a contradiction.  I then realized that if 
+I defined the circumstances, act, and goal as constant, then FUL0 was indeed consistent. After some 
+experiementation, Prof. Amin correctly pointed out that, as constants, these three entities were 
+distinct. When merely quantifying over (c, a, g), all members of a tuple could be equivalent. Within
+a minute, I could formalize this notion, add it to FUL0, and test if it solved the problem. The fact 
+that it did spurred my philosophical insight about vacuous maxims. 
+
+The logic confirmed that certain kinds
+of circumstance, act, goal tuples are too badly formed for the categorical imperative to logically 
+apply to them. The realization of this subtle problem would have been incredibly difficult without 
+computational tools. The syntax and typing of Isabelle/HOL forced me to bind the free-variable $M$
+in the FUL in different ways and allowed me to quickly test many bindings. The discovery of this 
+logical inconsistency then enabled a philosophical insight about which kinds of maxims make sense as 
+practical principles. This is one way to do computational ethics: model a system in a logic, use 
+computational tools to refine and debug the logic, and then use insights about the logic to derive 
+insights about the ethical phenonema it is modelling. This procedure parallels the use of proofs in 
+theoretical math to understand the world.\<close>
+
+text \<open>One potential problem with my formalization is that it does not hugly use the modal nature of the system. 
+All of the properties that the FUL investigates hold at all worlds, in effect removing the modal nature 
+of the system. This approach simplifies logical and therefore computational complexity, improving 
+performance. On the other hand, it doesn't use the full expressivity of DDL. If I run into problems 
+later on, one option is to tweak the FUL to use this expressivity. \<close> 
+
+(*<*)
+
+  text \<open>This is a formulation of the FUL in which, assuming every maxim is universalized at some
+world (this is the axiom imagination works), at that world (or other worlds like it), if the maxim is 
+not effective for the agent, then it is prohibited at the current world cw. Couple of optimizations 
+necessary to get Nitpick able to handle this:
+(1) Couldn't use the effective and will functions becuase they made Nitpick time out. Is there a way 
+to have a purely syntactic version of these functions? They'll make notation prettier? Like an 
+abbreviation with free variables?
+
+(2) If I quantify over the maxim's components, then Nitpick times out. If I specify a 
+single maxim as a constant, then it can handle everything just fine. I could try, every 
+time I'm working with an example, specifying properties of these constants. The downside is that if I want 
+to work with 2 maxims at once, then I have to add more constants and add the FUL as an axiom again for each of these maxims. This might not 
+be too annoying in practice (I don't think I'll need that many maxims), but there's something theoretically
+unsatisfying about it. I guess one theoretical explanation is that the system can only handle small models?
+Which is maybe not a huge problem?
+
 
 One worry is the order of the universal quantifier and the $\vDash$ or derivability symbol. In this 
 representation, the universal quantifier is tightly bound to the left hand side of the equation, so it 
@@ -228,85 +335,47 @@ below (using simplified versions of the representations) show that my representa
 the representation where the quantifier has larger scope. This seems promising—the representation in 
 lemma test\_2 is likely too strong because the quantifier quantifies into $M a$ as well. It really 
 doesn't seem like this should make a difference, since $p$ never appears on the RHS expression. Let's
-see if Prof. Amin has insight.
-\<close>
-
-abbreviation FUL0::bool where "FUL0 \<equiv> \<forall> c a g s. not_universalizable (c, a, g) s \<longrightarrow> \<Turnstile>((O{(\<^bold>\<not> W (c, a, g) s)| c}))"
-\<comment>\<open>This representation of the Formula of Universal Law reads, ``For all circumstances, goals, acts, 
-and subjects, if the maxim of the subject performing the act for the goal in the circumstances is not 
-universalizable (as defined above), then, at all worlds, in those circumstances, the subject 
-is prohibited (obligated not to) from willing the maxim.
-
-It's also inconsistent!!!! Boooooo\<close>
-
-lemma "FUL0 \<longrightarrow> False" using O_diamond ax_5d 
-  using prod.simps(2) split_conv by fastforce
+see if Prof. Amin has insight.\<close>
 
 abbreviation imagine_a_world::"maxim\<Rightarrow>bool" where 
-"imagine_a_world \<equiv> \<lambda>M. (\<exists>w. (\<forall>p. (W M p) w))"
-\<comment>\<open>The FUL really says, `imagine a world where the maxim were universalized...' I suspect that part of 
-the problem with FUL0 is that it universalizes the maxim at the same world that prohibits it, which 
-is weird because the agent performs the action (since it's universalized) but it's prohibited. Instead, 
-we want to say that in some other world where the maxim is universalized, let's apply the universalizability 
-test. This particular function represents a world where the maxim is universalized.\<close>
+"imagine_a_world \<equiv> \<lambda>M. (\<exists>w. \<not> w = cw \<and> (\<forall>p. (W M p) w))"
+\<comment>\<open>This abbreviation formalizes the idea that, for any maxim, some world where exist where the maxim 
+is universally willed.\<close>
 
 abbreviation not_contradictory::"maxim\<Rightarrow>bool" where 
 "not_contradictory \<equiv> \<lambda>(c, a, g). (\<forall>p w. \<not> ((c \<^bold>\<and> (a p)) \<^bold>\<rightarrow> \<^bold>\<bottom>) w)"
-\<comment>\<open>This function represents a maxim that is not contradictory, in that an agent can will it without 
-contradiction. It's more of a sanity check.\<close> 
+\<comment>\<open>This abbreviation formalizes the idea of a non-contradictory maxim, defined as a maxim such that 
+no matter who wills it at what world, the combination of the circumstances and the action never 
+lead to a contradiction.\<close> 
 
-axiomatization where imagination_works:"\<forall>M::maxim.  (not_contradictory M) \<longrightarrow>  (\<exists>w. (\<forall>p. (W M p) w))"
+text \<open>axiomatization where imagination_works:"$\forall$M::maxim.  (not_contradictory M) \<longrightarrow>  (imagine_a_world M)"\<close>
 \<comment>\<open>This axiom says, effectively, that our imagination works. In other words, for every maxim, if it 
 isn't contradictory, there is some (imagined) world where it is universalized. This guarantees that the 
-FUL can't be trivially true, since there is some world where the universalizability test is carried out.\<close>
+FUL can't be vacuously true, since there is some world where the universalizability test is carried out.\<close>
 
 lemma True
   nitpick[user_axioms, falsify=false] by simp 
-\<comment>\<open>Consistent so far:
-Nitpick found a model for card i = 1 and card s = 1:
+\<comment>\<open>Asserting that we can successfully imagine a non-contradictory maxim to be universalized still results
+in a consistent system.
+$\color{blue}$ Nitpick found a model for card i = 1 and card s = 1:
 
-  Empty assignment\<close>
+  Empty assignment $\color{black}$\<close>
+abbreviation not_universalizable_modal::"maxim\<Rightarrow>s\<Rightarrow>bool"
+  where "not_universalizable_modal \<equiv> \<lambda>M s. (\<forall>w. \<forall>p. (W M p) w \<longrightarrow> \<not> E M s w)"
 
-abbreviation not_vacuous::"maxim\<Rightarrow>s\<Rightarrow>bool" where 
-"not_vacuous \<equiv> \<lambda>(c, a, g) s. \<not> (g = c) \<and> \<not> (a s = c)"
+text \<open>To fix this, I return to the text of @{cite KorsgaardFUL}$\footnote{p. 20}$, in which she describes the universalizability 
+test as taking place in an ``imagined" world where the maxim is universalized. In other words, the test
+does not require that the maxim is actually universalized in reality in the current universe. Instead, 
+we imagine that it is universalized in some parallel universe and study that alternate universe to 
+understand what our obligations are in the real world. Under this reading, the maxim should be universalized
+at some other world, and then we should decide whether or not it is obligatory at the current world. 
 
-abbreviation FUL where "FUL \<equiv> \<forall>M::maxim. \<forall>s::s. (not_vacuous M s) \<longrightarrow> (not_universalizable M s \<longrightarrow> O {\<^bold>\<not> (will M s)} cw)"
-
-lemma "FUL"
-  nitpick[user_axioms, falsify=true] oops
-\<comment>\<open>$\color{blue}$ Nitpick found a counterexample for card s = 1 and card i = 1:
-
-  Skolem constants:
-    a = ($\lambda x. \_$)($s_1$ := ($\lambda x. \_$)($i_1$ := False))
-    c = ($\lambda x. \_$)($i_1$ := True)
-    g = ($\lambda x. \_$)($i_1$ := False)
-    $\lambda w$. p = ($\lambda x. \_$)($i_1$ := $s_1$)
-    s = $s_1$ $\color{black}$ \<close>
-
-axiomatization where FUL:FUL
-
-lemma True
-  nitpick[user_axioms, falsify=false] by simp
-\<comment>\<open>$\color{blue}$ Nitpick found a model for card i = 1 and card s = 1:
-
-  Empty assignment $\color{black}$ \<close>
-
-  text \<open>This is a formulation of the FUL in which, assuming every maxim is universalized at some
-world (this is the axiom imagination works), at that world (or other worlds like it), if the maxim is 
-not effective for the agent, then it is prohibited at the current world cw. Couple of optimizations 
-necessary to get Nitpick able to handle this:
-(1) Couldn't use the effective and will functions becuase they made Nitpick time out. Is there a way 
-to have a purely syntactic version of these functions? They'll make notation prettier? Like an 
-abbreviation with free variables?
-
-(2) If I quantify over the maxim's components, then Nitpick times out. If I specify a 
-single maxim as a constant, then it can handle everything just fine. I could try, every 
-time I'm working with an example, specifying properties of these constants. The downside is that if I want 
-to work with 2 maxims at once, then I have to add more constants and add the FUL as an axiom again for each of these maxims. This might not 
-be too annoying in practice (I don't think I'll need that many maxims), but there's something theoretically
-unsatisfying about it. I guess one theoretical explanation is that the system can only handle small models?
-Which is maybe not a huge problem?\<close>
-
-(*<*)
+To formalize this, I will first add as an axiom the idea that every non-contradictory maxim is 
+universalized at some world. In order for the test to actually be able to find a world where the 
+maxim is universalized, we need to imagine that such a world exists. Practically, this axiom asserts
+that we are able to successfully imagine a world where the maxim is universalized. Notice that this 
+axiom can only hold for non-contradictory maxims, else we would have a world where a contradiction holds 
+and the logic would become inconsistent! This coheres with basic moral intuitions as well—asking if 
+a contradictory maxim is morally prohibited is, in effect, asking an incoherent question. Garbage in, garbage out!\<close>
 end
 (*>*)
