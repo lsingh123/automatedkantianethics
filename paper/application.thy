f38ae6eb7e436ddcee5cb7b53062db90e96675d7
Lavanya Singh <lsingh@college.harvard.edu>
2021-11-21 14:33:02 -0500

    did some writing

--- paper/application.thy
+++ paper/application.thy
@@ -12,7 +12,8 @@ ethics in helping philosophers think about ethical dilemmas. Second, I will enco
 facts into the system and then test the results of applying this more powerful system to common
 ethical dilemmas. This section will serve as a prototype of the kind of reasoning that this kind of 
 computational ethics can do and will demonstrate the utility of computational ethics in performing 
-everyday ethical reasoning. Finally, I will also philosophically analyze the possibility and value of 
+everyday ethical reasoning. This section will also outline what future work is necessary to use an 
+ethical reasoner like the one I build in practice. Finally, I will also philosophically analyze the possibility and value of 
 applications like these. Specifically, I will consider to what extent humans should let computers do 
 ethical reasoning on our behalf. \<close>
 
@@ -82,7 +83,70 @@ which is quite close to the idea of the maxim not being universalizable.\<close>
     using FUL assms(2) by blast 
 qed
 
-text \<open>\<close>
+text \<open>The above is an example of how my system can perform ``applied ethical reasoning." In Chapter 2
+and Chapter 3, I performed metaethical reasoning while testing different formalizations of the FUL. Metaethical
+reasoning analyzes properties of moral thought itself \citep{sepme}. Because metaethics is about ethics itself,
+and not about the actual dilemmas that ethics is supposed to help us resolve, this kind of reasoning requires
+very little additional knowledge beyond that of the ethical theory in question. Indeed, in previous chapters,
+I perform metaethical reasoning using my system (which formalizes an ethical theory) and basic logical
+facts and objects. This kind of reasoning contrasts with ``applied ethical reasoning," which is the use of
+ethics to actually resolve ethical dilemmas and make judgements about what an agent should or should not do.
+Applied ethical reasoning is about the situation an agent is in, not about an ethical theory as a whole,
+abstract entity. In this section, I expore how my system can perform extended applied ethical reasoning. 
+Metaethical reasoning is most interesting to philosophers who are trying to formulate the ``best" or 
+most correct theory of ethics. Applied ethical reasoning, on the other hand, is useful to ordinary people
+who are trying to decide how to live their lives. These two kinds of reasoning represent the two motivations 
+for automated ethics. Metaethical reasoning is more directly a way to perform computational ethics, or use a computer
+system to do philosophy. Applied ethical reasoning is also relevant to philosophers (applied ethicists 
+generally perform this kind of reasoning), but is also relevant to AI agents or people seeking to navigate the world.
+In order for automated ethics to guide AI agents, it must perform applied ethical reasoning. 
+
+One challenge of applied ethical reasoning is that it requires more common sense knowledge than metaethical
+reasoning. Applied ethical reasoning focuses on a particular ethical dilemma and thus requires enough 
+common sense to understand the dilemma and options at hand. For example, for an applied ethicist to 
+evaluate the permissibility of lying, they need some robust definition of the term lying and likely some
+understanding about the activities of communication and truth telling. In this section, I attempt to
+endow my system with this kind of common sense in the specific case of lying. Indeed, in order for my 
+system to be used to perform applied ethical reasoning, it will need to be equipped with a database
+of common sense facts and definitions as I present in this section. My system will contribute the core
+reasoning about the Formula of Universal Law, but this reasoning must be applied to objects that are defined
+in this common sense database. In this section, I try to understand what this common sense reasoning looks like
+for the specific example of lying. I will examine how different sets of common sense facts impact the 
+judgements that my system comes to. 
+
+Because these common sense facts can entirely determine my system's judgement, they are part of the trusted
+code base for my system. In order for someone to trust my system's judgements, they must trust that the
+common sense database is correct. This common sense reasoning is also not a part of my system—in order for my
+system to be used, someone must develop a common sense database for the particular situation at hand. 
+The challenge of endowing automated ethical reasoners with common sense reasoning is not unique to my 
+system, and virtually all prior attempts in machine ethics face similar challenges.
+
+The specific kind of common sense reasoning required for automated Kantian ethics appears to be a challenge 
+for automating Kantian ethics, and perhaps a reason that consequentialist or virtue ethical automated
+agents would be within closer reach for automation. Ultimately, I still argue that Kantian ethics is 
+easiest to automate because it will require fewer, less controversial common facts than other ethical
+systems. As this example demonstrates, Kantian ethics requires a definition of lying (which any other 
+theory would also requires) and the knowledge that if everyone lies in a given context, no one will believe 
+each other in that particular context. This latter fact may not be required for every ethical theory, 
+but is relatively uncontroversial. Indeed, Korsgaard doesn't even bother to give a justification for 
+this fact. It is a kind of intuition about human behavior that is generally accepted. Neither a definition 
+of lying nor this property of lying seem like unreasonable prerequisites for ethical reasoning. 
+
+Consequentialism, on the other hand, would require much more specific data about the consequences of 
+a lie, perhaps for the specific person's credibility, for the victim of the lie, for the third-parties 
+watching the lie unfold. Indeed, consequentialism more numerous and specific judgements, all of which are 
+likely to be more controversial than the two outlined above for Kantian ethics. Similarly, virtue ethics
+would likely require information about the actor's entire moral character, including their attitude
+towards the lie and their moral history. Virtue ethics would also require robust definitions of the 
+relevant virtues, in addition to a definition of lying. Thus, while Kantian ethics requires some 
+common sense reasoning, it requires fewer and less controversial background facts than other ethical theories.
+
+This section will provide additional examples of the kinds of commons sense required to get my system
+off the ground. I will aim to use as lean and uncontroversial of a common sense database as possible
+to achieve results as robust and powerful as possible. This serves as evidence for the ease of automating
+Kantian ethics, an example of what additional work my system requires, and a demonstration of the contributions
+that I make.
+   \<close>
 
 consts believe::"s\<Rightarrow>t\<Rightarrow>t"
 definition lie::"s\<Rightarrow>t\<Rightarrow>t" where "lie person statement \<equiv> (say person statement) \<^bold>\<and> (\<^bold>\<not> (believe person statement))"
